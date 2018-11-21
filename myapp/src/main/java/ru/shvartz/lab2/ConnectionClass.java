8b62eaca65abc508d8cbec33ec5a2093f38b00f0
Дарья Чернова <dariashvartz@mail.ru>
2018-11-21 20:45:52 +0300

    clean the app

--- myapp/src/main/java/ru/shvartz/lab2/ConnectionClass.java
+++ myapp/src/main/java/ru/shvartz/lab2/ConnectionClass.java
@@ -1,34 +1,52 @@
 package ru.shvartz.lab2;
 
 import java.sql.*;
-
+import java.util.Scanner;
 
 public class ConnectionClass {
 
+    private static final String insertIntoTable = "INSERT INTO users (id, name, email) VALUES(?,?,?)";
+    private static final String selectFromTable = "SELECT * FROM users  LIMIT 10";
+    private static final String deleteFromTable = "DELETE FROM users where id = ?";
+    private static final String updateTableName = "UPDATE users SET name = ? where id = ?";
+
     public static void main(String[] args) throws SQLException {
-        workingSQL();
-    }
+        Connection connection = ConnectionModel.getDBConnection();
+        Scanner scan = new Scanner(System.in);
 
-    public static void workingSQL() throws SQLException {
-        Connection connection = getDBConnection();
+        System.out.println("Введите команду");
+        System.out.println("1 - insert, 2 - update, 3 - select, 4 - delete");
+
+       int command = 0;
+
         try {
-            sqlCommands commands = new sqlCommands();
-            sqlTasks.doCommand(connection, commands.getDropDatabaseString());
-            System.out.println("database was dropped");
-            sqlTasks.doCommand(connection, commands.getCreateDataBaseString());
-            System.out.println("database was created");
-            sqlTasks.doCommand(connection, commands.getUseDatabase());
-            sqlTasks.doCommand(connection, commands.getCreateCourse());
-            sqlTasks.doCommand(connection, commands.getHack());
-            sqlTasks.doCommand(connection, commands.getCreateUser());
-            sqlTasks.doCommand(connection, commands.getAlterTable());
-            sqlTasks.doCommand(connection, commands.getDeleteAllFromUser());
-            sqlTasks.doCommand(connection, commands.getDeleteAllFromCourse());
-            sqlTasks.doCommand(connection, commands.getInsertTableUser());
-            sqlTasks.doCommand(connection, commands.getInsertTableCourse());
-
-            sqlTasks.select(connection,"user");
-
+            do {
+                command = scan.nextInt();
+                switch (command) {
+                    case 1: {
+                        System.out.println("Введите количество записей");
+                        int i = scan.nextInt();
+                        int count = 0;
+                        while (count < i) {
+                            insertTable(connection);
+                            count++;
+                        }
+                        break;
+                    }
+                    case 2: {
+                        updateTable(connection);
+                        break;
+                    }
+                    case 3: {
+                        selectTable(connection);
+                        break;
+                    }
+                    case 4: {
+                        deleteTable(connection);
+                        break;
+                    }
+                }
+            } while (command != 0);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
@@ -36,21 +54,73 @@ public class ConnectionClass {
         }
     }
 
-    public static Connection getDBConnection() {
-        ConnectionModel connectionModel = new ConnectionModel("localhost", 3306, "lab2", "root", "AkwcEcsE");
-        Connection connection = null;
+
+
+    public static void insertTable(Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = null;
+        Scanner scan = new Scanner(System.in);
         try {
-            Class.forName(connectionModel.getDriverName());
-        } catch (ClassNotFoundException e) {
-            System.out.println(e.getMessage());
-        }
-        try {
-            connection = DriverManager.getConnection(connectionModel.getUrl(), connectionModel.getUser(), connectionModel.getPassword());
-            return connection;
+            System.out.println("Введите id пользователя");
+            int idUser = scan.nextInt();
+            System.out.println("Введите имя пользователя");
+            String nameUser = scan.next();
+            System.out.println("Введите mail пользователя");
+            String mailUser = scan.next();
+            preparedStatement = connection.prepareStatement(insertIntoTable, preparedStatement.RETURN_GENERATED_KEYS);
+            preparedStatement.setInt(1, idUser);
+            preparedStatement.setString(2, nameUser);
+            preparedStatement.setString(3, mailUser);
+            preparedStatement.execute();
+            preparedStatement.getGeneratedKeys();
+            System.out.println("Table was created");
         } catch (SQLException e) {
+            System.out.println("CreateTable method failed");
             System.out.println(e.getMessage());
         }
-        return connection;
     }
 
+    public static void updateTable (Connection connection) throws SQLException{
+        Scanner scan = new Scanner(System.in);
+        PreparedStatement preparedStatement = connection.prepareStatement(updateTableName);
+        System.out.println("Введите id пользователя");
+        System.out.println("Введите новое имя пользователя");
+        try {
+            preparedStatement.setInt(2, scan.nextInt());
+            preparedStatement.setString(1, scan.next());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("updateTable failed");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void selectTable(Connection connection) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(selectFromTable);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        try {
+            while (resultSet.next()) {
+                System.out.print(resultSet.getInt(1) + "|");
+                System.out.print(resultSet.getString(2) + "|");
+                System.out.println(resultSet.getString(3) + "|");
+            }
+        } catch (SQLException e) {
+            System.out.println("selectTable failed");
+            System.out.println(e.getMessage());
+        }
+    }
+
+     public static void deleteTable(Connection connection) throws  SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(deleteFromTable);
+        System.out.println("Введите id пользователя");
+        Scanner scan = new Scanner(System.in);
+
+        try {
+            preparedStatement.setInt(1, scan.nextInt());
+            preparedStatement.execute();
+        } catch(SQLException e) {
+            System.out.println("deletetable was failed");
+            System.out.println(e.getMessage());
+        }
+     }
 }
--- myapp/src/main/java/ru/shvartz/lab2/ConnectionModel.java
+++ myapp/src/main/java/ru/shvartz/lab2/ConnectionModel.java
@@ -1,5 +1,9 @@
 package ru.shvartz.lab2;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionModel {
     private String host;
     private int port;
@@ -9,7 +13,6 @@ public class ConnectionModel {
     private String url;
     private String driverName;
 
-
     public ConnectionModel(String host, int port, String database, String user, String password) {
         this.host = host;
         this.port = port;
@@ -17,19 +20,29 @@ public class ConnectionModel {
         this.user = user;
         this.password = password;
         this.url = "jdbc:mysql://" + host + ":" + port + "/" + database + "?serverTimezone=UTC";
-        this.driverName = "com.mysql.cj.jdbc.Driver";
+        this.driverName = "com.mysql.jdbc.Driver";
     }
 
-    public String getUser() {
-        return user;
+    public String getUser() { return user; }
+    public String getPassword() { return password; }
+    public String getUrl() { return url; }
+    public String getDriverName() { return driverName; }
+
+    public static Connection getDBConnection() {
+        ConnectionModel connectionModel = new ConnectionModel("localhost", 3306, "lab2", "root", "AkwcEcsE");
+        Connection connection = null;
+        try {
+            Class.forName(connectionModel.getDriverName());
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            connection = DriverManager.getConnection(connectionModel.getUrl(), connectionModel.getUser(), connectionModel.getPassword());
+            return connection;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return connection;
     }
-    public String getPassword() {
-        return password;
-    }
-    public String getUrl() {
-        return url;
-    }
-    public String getDriverName() {
-        return driverName;
-    }
+
 }
